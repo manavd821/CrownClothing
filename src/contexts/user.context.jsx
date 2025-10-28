b44346128604e95fa2fbec95e8bae1cf6048bdad
manavok <mitmanav6805@gmail.com>
2025-10-28 10:49:55 +0530

    Updating the user context by replacing useState to useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,9 @@
-import { createContext, useState, useEffect } from "react";
+import { 
+    createContext, 
+    useState, 
+    useEffect, 
+    useReducer,
+} from "react";
 import { onAuthStateChangeListener } from "../utils/firebase/firebase.utils";
 import { createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 // as the actual value you want access
@@ -6,9 +11,37 @@ export const UserContext = createContext({
     currentUser : null,
     setCurrentUser : () => null
 });
+export const USER_ACTION_TYPE = {
+    "SET_CURRENT_USER" : 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    // console.log('dispatched')
+    // console.log(action);
+    const {type, payload} = action;
+    switch(type){
+        case USER_ACTION_TYPE.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser : payload,
+            };
+        default:
+            throw new Error(`Unhanled type ${type} in useReducer`);
+    }
+};
+const INITIAL_STATE = {
+    currentUser : null,
+}
 
 export const UserProvider = ({ children }) =>{
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const {currentUser} = state;
+    // console.log(currentUser);
+    const setCurrentUser = (user) => {
+        dispatch({type : USER_ACTION_TYPE.SET_CURRENT_USER, payload : user})
+    }
     const value = {currentUser, setCurrentUser};
 
     useEffect(() => {
