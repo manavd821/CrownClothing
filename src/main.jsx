4f714d59b3acb2eb61f4842390bdc95d206d19e7
manavok <mitmanav6805@gmail.com>
2025-11-14 08:49:49 +0530

    remove unnecessory imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
-import { CategoriesProvider } from './contexts/categories.context.jsx';
-import { UserProvider } from './contexts/user.context.jsx';
-import {UserCartProvider} from './contexts/userCart.context.jsx';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
